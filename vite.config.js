15131e5375a89cf5a889e1e3bc9ac7d97ab14960
Lakshmi Chinnahalli <31415581+lakshmichinnahalli@users.noreply.github.com>
2025-07-25 20:58:36 +0100

    Move React app to repository root and update paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TravelAdvisorFrontend/docs/', // Set the base path for GitHub Pages
+  base: '/TravelAdvisorFrontend/', // Set the base path for GitHub Pages
 })
